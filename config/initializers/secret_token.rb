02db2e7956c109254e3e33089cc94f9cd4d54cac
Brian Sherman <bsherman26@hotmail.com>
2012-09-11 23:42:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EagleEco::Application.config.secret_token = 'a7a3aeff40769c2a3b0d75b7740232c53ea0f59417f3de3208112f30e3decf53b2ac4279b86d5bc8690498fa449b5817bb1d887acd82f5f9798a5c3e9590c9af'
